d0321682ff6efcfa0003b7622daa3d975d2feade
Vitor Aoki <vitoraoki17@gmail.com>
2023-09-14 17:11:45 -0300

    requirement 06 - implementing reverse method from translate controller

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -49,4 +49,26 @@ def index():
 # Req. 6
 @translate_controller.route("/reverse", methods=["POST"])
 def reverse():
-    raise NotImplementedError
+    languages = LanguageModel.list_dicts()
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+    translated = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
+    HistoryModel(
+        {
+            "text_to_translate": text_to_translate,
+            "translate_from": translate_from,
+            "translate_to": translate_to,
+        }
+    ).save()
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=translated,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=text_to_translate,
+    )
