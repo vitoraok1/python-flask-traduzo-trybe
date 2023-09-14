2aabcfc19d49ab8759665731e8ed194f1491d8bc
Vitor Aoki <vitoraoki17@gmail.com>
2023-09-14 18:34:48 -0300

    requirement 07 - implementing test from history model

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,25 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    json_response = json.loads(HistoryModel.list_as_json())
+
+    for item in json_response:
+        item.pop("_id", None)
+
+    expected_response = [
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+        {
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt",
+        }
+    ]
+
+    assert json_response == expected_response
